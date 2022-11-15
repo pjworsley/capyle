475e4aa4adfec4547fadec1f194dcf217c3ff790
Aleksander Osikowicz <amosikowicz1@sheffield.ac.uk>
2022-11-15 13:32:23 +0000

    Cell class

---
+++ ca_descriptions/cell.py
@@ -0,0 +1,65 @@
+
+from dataclasses import dataclass
+from enum import Enum
+
+
+class State(Enum):
+    NORMAL = 0
+    BURNING = 1
+    DEAD = 2
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Type(Enum):
+    CHAPARRAL = 0
+    CANYON = 1
+    FOREST = 2
+    LAKE = 3
+    TOWN = 4
+
+    def __str__(self) -> str:
+        return self.name
+
+    def get_fuel(self):
+        FUEL = {
+            self.CANYON: 1,
+            self.CHAPARRAL: 28,
+            self.FOREST: 120,
+        }
+        return FUEL[self]
+
+
+@dataclass
+class Cell(object):
+    def __init__(self, type: Type) -> None:
+        self.type = type
+        self.fuel = self.type.get_fuel()
+        self.state = State.NORMAL
+
+    def __str__(self):
+        return f"""CELL
+        Type: {self.type} 
+        State: {self.state}
+        Fuel: {self.fuel}  
+        """
+
+    def update(self, ignite: bool):
+
+        # cell is dead if ran out of fuel
+        if self.fuel == 0:
+            self.state = State.DEAD
+
+        # ignite the cell
+        if ignite and self.state == State.NORMAL:
+            self.state = State.BURNING
+
+        # decrease fuel when burning
+        if self.state == State.BURNING:
+            self.fuel -= 1
+
+
+x = Type.CHAPARRAL
+print(x.get_fuel())
+print(Cell(Type.CANYON))
